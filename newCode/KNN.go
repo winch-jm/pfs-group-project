d6b69528c2d05e97222c78d4423bfba6cab27d7e
nethanr <nethanr@andrew.cmu.edu>
2025-11-22 15:15:43 -0500

    everything up until maxHeap

--- newCode/KNN.go
+++ newCode/KNN.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	// "container/heap"
-	// "crypto/x509"
+	"container/heap"
+	"crypto/x509"
 	"encoding/csv"
 	"fmt"
 	"io"
@@ -15,6 +15,42 @@ type DenseRows struct {
 	// n rows (cells) × d columns (genes or features)
 	N, D int
 	Data []float32 // row-major: row i at Data[i*D:(i+1)*D]
+}
+
+
+
+
+// MaxHeap is a max-heap of ints.
+type MaxHeap []int
+
+func (h MaxHeap) Len() int           { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] } // NOTE: '>' for max-heap
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+// Push and Pop use pointer receiver because they modify the slice.
+func (h *MaxHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MaxHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
+
+
+
+func Cosine(a, b []float32) float32 {
+
+	var s float32
+	// dot product
+	for i := range a {
+		s += a[i]*b[i]
+	}
+
+	return s
 }
 
 func main() {
@@ -65,8 +101,39 @@ func main() {
 
 	fmt.Printf("Loaded matrix with %d rows and %d columns\n", file_dr.N, file_dr.D)
 	fmt.Printf("Data slice length: %d (should be N*D = %d)\n",
-		len(file_dr.Data), file_dr.N*file_dr.D)
+	len(file_dr.Data), file_dr.N*file_dr.D)
+
+
+
+
 
 }
+
+func Similarities(dr DenseRows) {
+	
+
+
+	for i:=0;i<dr.D;i++{
+		
+		for j:=0;j<dr.D;j++{
+			tempCosineSimilarity:=make([]float32,0)
+			make newMaxHeap(make)
+			if i!=j{
+				dataSet1:= dr.Data[i*dr.D:(i+1)*dr.D]
+				dataSet2:= dr.Data[j*dr.D:(j+1)*dr.D]
+				metric:= Cosine(dataSet1,dataSet2)
+				tempCosineSimilarity = append(tempCosineSimilarity, metric)
+			}
+			// top3 he
+
+			
+		}
+	}
+}
+
+func GraphCreation(){
+
+}
+
 
 // First thing: Turn CSV into denserows object
--- newCode/go.mod
+++
@@ -1,5 +0,0 @@
-module pfs-knn
-
-go 1.24.4
-
-require github.com/sjwhitworth/golearn v0.0.0-20221228163002-74ae077eafb2 // indirect
