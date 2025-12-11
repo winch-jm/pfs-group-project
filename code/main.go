639f0560a0b91c0d7ab2a5dba387634066f14073
Jeff Winchell <winch.jm@gmail.com>
2025-12-11 12:59:42 -0500

    grid search in own function

--- code/main.go
+++ code/main.go
@@ -4,49 +4,48 @@ package main
 
 import(
 	// "fmt"
-	// "log"
-	// "strconv"
+	"log"
+	"strconv"
+	// "os"
 )
 
-func main(){
-
-	// Preprocess 
-	// // - load dense rows
-	file := "../data/mcf7_subset_pca.csv"
-
-	kList := []int{10,15,20,25} 
-	// resList := []float32{0.25,0.5,0.75,1.0,1.25,1.5}
+func RunGridSearch(dataset DenseRows, saveDir, prefix string, kList []int, resList []float32, sigIds []string) {
+	
 	for _,k := range kList {
-		_, dataset := CSVToDenseRows(file)
-			
-		// - L2 normalize
-		dataset = NormalizeDenseRows(dataset)
 
 		// build KNN (output is a CSR)
+		g := WeightedKNN(dataset, k)
 
-		WeightedKNN(dataset, k)
-		// fmt.Println(g)
-		
-	// 	//free memory from denseRows (no longer needed)
-	// 	dataset = DenseRows{}
-	// 	//graph does not change after KNN!!
+		for _, gamma := range resList {
 
-	// 	for _, gamma := range resList {
-
-	// 		// Leiden algorithm
-	// 		P, levelStats := LeidenCommunityDetection(g, RBPM, 0.001, gamma, 10, 10, 32)
-	// 		fmt.Println(len(P),P)
-	// 		fmt.Println(levelStats)
-
-			
-	// 		// ... run leiden, get origP ...
-	// 		if err := WriteFinalPartitionCSV(sigIds, P, "../data/gridsearch/partition_mcf7_k" + strconv.Itoa(k)+"_g"+strconv.Itoa(int(gamma*100))+".csv"); err != nil {
-	// 			log.Fatal(err)
-	// 		}
-			
-	// 	}
-	// 	g.WriteEdgeListCSV("../data/gridsearch/graph_edges_mcf7_k"+ strconv.Itoa(k)+".csv",true)
+			// Leiden algorithm
+			P, levelStats := LeidenCommunityDetection(g, RBPM, 0.001, gamma, 10, 10, 32)
+			print(levelStats)
+			if err := WriteFinalPartitionCSV(sigIds, P, saveDir+"/gridsearch/"+prefix+"_partition_k" + strconv.Itoa(k)+"_g"+strconv.Itoa(int(gamma*100))+".csv"); err != nil {
+				log.Fatal(err)
+			}
+		}
+		g.WriteEdgeListCSV(saveDir +"/gridsearch/"+prefix+"_graph_edges_k"+ strconv.Itoa(k)+".csv",true)
 	}
+}
+
+func main() {
+	// params := os.Args
+	file := "../data/mcf7_subset_pca.csv"
+	prefix := "mcf7"
+	// ./code filename 
+	// fmt.Println(params)
 	
+	sigIds, dataset := CSVToDenseRows(file)
+			
+		// - L2 normalize
+	dataset = NormalizeDenseRows(dataset)
+	// Preprocess 
+	// // - load dense rows
+	
+	kList := []int{10,15,20,25} 
+	resList := []float32{0.25,0.5,0.75,1.0,1.25,1.5}
+	saveDir := ""
+	RunGridSearch(dataset, saveDir, prefix, kList, resList, sigIds)
 
 }
