b52728fd65f5607aabcc030be41dc921c5ed4c5d
ajayprab20 <ajayprab5293@gmail.com>
2025-12-12 17:36:04 -0500

    final adjustments to commenting and code

--- code/main.go
+++ code/main.go
@@ -6,7 +6,7 @@ import(
 	// "fmt"
 	"log"
 	"strconv"
-	// "os"
+	"os"
 )
 
 func RunGridSearch(dataset DenseRows, saveDir, prefix string, kList []int, resList []float32, sigIds []string) {
@@ -31,23 +31,28 @@ func RunGridSearch(dataset DenseRows, saveDir, prefix string, kList []int, resLi
 
 func main() {
 	// params := os.Args
-	file := "../data/mcf7_subset_pca.csv"
-	prefix := "mcf7"
-	// ./code filename 
-	// fmt.Println(params)
-	
+	//file := "../data/mcf7_subset_pca.csv"
+	file := os.Args[1] //First argument is an input of CSV file
+	projectName := os.Args[2]//Second argument is the prefix tagged onto images
+	outputDir := os.Args[3]//Third argument is the output directory for plots
+	gridSearch := os.Args[4]//Fourth argument is whether user wants to run GridSearch to find optimal k and gamma
 	sigIds, dataset := CSVToDenseRows(file)
-			
-		// - L2 normalize
 	dataset = NormalizeDenseRows(dataset)
-	// Preprocess 
-	// // - load dense rows
-	
-	kList := []int{10,15,20,25} 
-	resList := []float32{0.25,0.5,0.75,1.0,1.25,1.5}
-	saveDir := ""
-	RunGridSearch(dataset, saveDir, prefix, kList, resList, sigIds)
-
+	if gridSearch == "true"{ //Runs grid search and then runs the algorithm
+		kList := []int{10,15,20,25} 
+		resList := []float32{0.25,0.5,0.75,1.0,1.25,1.5}
+		RunGridSearch(dataset, outputDir, projectName, kList, resList, sigIds)
+	}else{ //If they don't specify gridSearch, user has to define k and gamma and run algorithm
+		k, _ := strconv.Atoi(os.Args[5]) //
+		gamma, _ := strconv.ParseFloat(os.Args[6], 64)
+		g := WeightedKNN(dataset, k)
+		P, levelStats := LeidenCommunityDetection(g, RBPM, 0.001, float32(gamma), 10, 10, 32)
+		print(levelStats)
+		if err := WriteFinalPartitionCSV(sigIds, P, outputDir+"/gridsearch/"+projectName+"_partition_k" + strconv.Itoa(k)+"_g"+strconv.Itoa(int(gamma*100))+".csv"); err != nil {
+			log.Fatal(err)
+		}
+		g.WriteEdgeListCSV(outputDir +"/gridsearch/"+projectName+"_graph_edges_k"+ strconv.Itoa(k)+".csv",true)
+	}
 }
 
 //command line parsing for input dataset,output directory if you are doing grid_search, if you want to run algorithm once specify k and gamma
